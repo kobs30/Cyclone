5e7b47dd65d59b0f8ae0c7c4fe18789a932cccea
Fishness-developer <83603777+Fishness-developer@users.noreply.github.com>
2024-09-18 13:50:24 +0300

    Update top_menu.js

--- js/top_menu.js
+++ js/top_menu.js
@@ -4,3 +4,24 @@ function openDialogSucsess(){
 	dialogSucsess.classList.remove('not-active')
 	dialogForm.classList.add('not-active')
 }
+
+function openDialogForm() {
+		 
+		document.getElementById("openDialog").classList.remove("close-form-container");
+      document.getElementById("openDialog").classList.add("open-form-container");
+	}
+
+	function closeDialofForm(){
+		document.getElementById("openDialog").classList.remove("open-form-container");
+      document.getElementById("openDialog").classList.add("close-form-container");
+	}
+	
+    document.addEventListener('DOMContentLoaded', () => {
+      const formPopup = document.getElementById('popupForm');
+      const successPopup = document.getElementById('successPopup');
+      const closeFormPopup = document.getElementById('closeFormPopup');
+      const closeSuccessPopup = document.getElementById('closeSuccessPopup');
+      const infinityForm = document.getElementById('infinityForm');
+		
+
+    });
